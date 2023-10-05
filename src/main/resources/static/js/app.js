1dd3cb0fda7c4058d568be2ff49643498b51828c
TeranRyl <pachitoteranroman@gmail.com>
2023-10-05 15:56:52 -0500

    Avance parte 3

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,9 +1,44 @@
-//
+import * as apimock from "apimock.js"
 
 var Module = (function () {
+    // Private variables
+    var author;
+    var list = [];
+    // Private method to access private variable
+    function changeName () {
+        author = $("#author").val();
+    }
 
-    var privateMethod = function () {
-        // do something
-    };
+    function fun() {
+        var blueprints = list.map(function(bp){
+            return {name: plano.name, points: plano.points.length};
+        });
+
+        $("#blueprints").find("td").remove();
+
+        blueprints.map(function(bp) {
+            var row = "<tr><td>" + bp.name + "</td><td>" + bp.points + "</td></tr>";
+            $("#blueprints").append(fila);
+        });
+
+        var totalPoints = list.reduce(function(total, bp) {
+            return total + bp.points.length;
+        }, 0);
+
+        $("#totalPoints").text(totalPoints);
+
+
+    }
+
+    function setList (author_new){
+        //changeName();
+        apimock.getBlueprintsByAuthor(author_new,fun);
+    }
+    // Public method that allows updating a private variable
+    return {
+        changeName: changeName,
+        setList: setList
+
+    }
 
 })();
